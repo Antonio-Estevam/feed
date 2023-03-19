8c1b5ef435ff9b70b5649d61ac8d105b2d4cdd58
antonio-estevam <jhonnyimmbe@gmail.com>
2023-03-19 20:46:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from'./App.module.css';
+
+import './global.css';
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+      <div className={styles.wrapper}>
+        
+        <Sidebar />
+
+        <main>
+          <Post
+            author = "Antonio Filho"
+            content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, maiores et? Totam soluta molestiae itaque repudiandae non? Quisquam quas possimus voluptatum maiores tempora minima corporis, consectetur, eaque, recusandae in quasi?"
+          />
+
+          <Post
+            author = "Lucas Silva"
+            content = "Maiores et? Totam soluta molestiae itaque repudiandae non? Quisquam quas possimus voluptatum maiores tempora minima corporis, consectetur, eaque, recusandae in quasi?"
+          />
+        </main>
+      </div>
+    </>
+  )
+}
